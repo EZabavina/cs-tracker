09d65c0c577c064e703f4ef6235caea4d5553314
EZabavina <ekaterina.zabavina@gmail.com>
2026-06-06 20:14:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
-/**
- * Скопируйте в config.js и подставьте ключи из Supabase → Project Settings → API.
- * config.js не коммитится в git.
- */
+
 const CST_CONFIG = {
     supabaseUrl: 'https://baynvciqzrabqyipwlfa.supabase.co',
     supabaseAnonKey: 'sb_publishable_oORCPugixc4WTUkfCkslYA_UM_8tzSd',
